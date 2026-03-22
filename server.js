3edcd1e8028d0305e0bb751361db9f7fe02fba88
opticalrecord <opticalrecords.ai@gmail.com>
2026-03-23 00:43:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,14 +28,12 @@ app.get('/search', async (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    sendUpdate();
+    socket.emit('update-queue', { list: queue });
 
-    // Sincronizza i VU-meter
     socket.on('audio-level', (level) => {
         socket.broadcast.emit('vibe-check', level);
     });
 
-    // Sincronizza il countdown
     socket.on('time-left', (seconds) => {
         socket.broadcast.emit('sync-countdown', seconds);
     });
@@ -43,23 +41,19 @@ io.on('connection', (socket) => {
     socket.on('add-to-queue', (song) => {
         if (!song || !song.id) return;
         queue.push(song);
-        sendUpdate();
+        io.emit('update-queue', { list: queue });
         if (queue.length === 1) io.emit('play-song', queue[0]);
     });
 
     socket.on('next-track', () => {
         if (queue.length > 0) {
             queue.shift();
-            sendUpdate();
+            io.emit('update-queue', { list: queue });
             if (queue.length > 0) io.emit('play-song', queue[0]);
             else io.emit('stop-player');
         }
     });
-
-    function sendUpdate() {
-        io.emit('update-queue', { list: queue });
-    }
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => console.log(`YOUTUBOX LIVE` || 3000));
+server.listen(PORT, '0.0.0.0', () => console.log(`YOUTUBOX ONLINE`));
